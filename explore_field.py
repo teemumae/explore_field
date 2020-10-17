d6e461a02a8b5f1f8dca56ee5931e593d1a5d9f0
Teemu Mäenpää <teemumaen@gmail.com>
2020-10-17 14:16:00 +0300

    explore_tile

--- explore_field.py
+++ explore_field.py
@@ -10,11 +10,14 @@ def explore_tile(c, row, column):
     Explore a tile - if there is an animal, prints the
     location and name of the animal
     """
+    if c!=" ":
+        print("Tile ({} , {} ) contains {}".format(row, column, ANIMALS[c]))
 def explore_field(p_field):
     """
     This function explores an entire field by calling the explore_tile
     function for each tile in the field.
     """
+
 field = [
     [" ", "s", " ", " ", "m"],
     [" ", "d", "@", "d", " "],
