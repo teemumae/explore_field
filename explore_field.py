74c9ad312931bd10ddef93e0ca5e041760115d11
Teemu Mäenpää <teemumaen@gmail.com>
2020-10-17 13:31:08 +0300

    Sanakirja ja funktionimet

--- explore_field.py
+++ explore_field.py
@@ -5,5 +5,13 @@ ANIMALS = {
     "m": "moogle",
     "c": "chocobo"
 }
-def explore_tile(c, column, row):
-
+def explore_tile(c, row, column):
+    """
+    Explore a tile - if there is an animal, prints the
+    location and name of the animal
+    """
+def explore_field(field):
+    """
+    This function explores an entire field by calling the explore_tile
+    function for each tile in the field.
+    """
